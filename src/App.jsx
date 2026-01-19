4255f0d5c1c96525a91af36791c720a3f6790490
Sakshi kirola <kirolasakshi@gmail.com>
2026-01-19 21:15:02 +0530

    This is Spotify-clone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebr from './Components/Sidebr'
+
+
+const App = () => { 
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebr/> 
+      </div> 
+    </div>
+  )
+}
+
+export default App
