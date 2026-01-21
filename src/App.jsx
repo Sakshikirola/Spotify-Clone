95f4e85ea22fbebe360489834c55df9082b4df98
Sakshi kirola <kirolasakshi@gmail.com>
2026-01-21 12:26:00 +0530

    Updated sidebar and album routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Sidebr from './Components/Sidebr'
+import Player from './Components/Player'
+import Display from './Components/Display'
 
 
 const App = () => { 
@@ -7,7 +9,9 @@ const App = () => {
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
         <Sidebr/> 
+        <Display/> 
       </div> 
+      <Player/>
     </div>
   )
 }
